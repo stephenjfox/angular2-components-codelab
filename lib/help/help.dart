4a838d01ac87e025966a3dc4811efc5fc9d0b7dc
Stephen Fox <stevethecoder34@gmail.com>
2017-01-28 20:35:27 -0700

    Add required dependencies to help page

--- lib/help/help.dart
+++ lib/help/help.dart
@@ -4,16 +4,20 @@
 
 import 'package:angular2/core.dart';
 import 'package:angular2/src/common/directives.dart';
+import 'package:angular2_components/angular2_components.dart';
 
 @Component(
   selector: 'help-component',
   templateUrl: 'help.html',
   styleUrls: const ['help.css'],
   directives: const [
+    materialDirectives,
     NgSwitch,
     NgSwitchWhen,
     NgSwitchDefault,
   ],
+  // providers aren't needed because we don't have the animations that something like
+  // MaterialProgressComponent requires
 )
 class HelpComponent {
   @Input()
